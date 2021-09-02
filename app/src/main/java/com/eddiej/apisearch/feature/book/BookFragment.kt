06da27a095375909ba3f28211196b03f30f65d7e
Eddie J <wonsam2477@gmail.com>
2021-09-02 22:52:15 +0900

    - 검색창 fragment_book 내부로 이동

--- app/src/main/java/com/eddiej/apisearch/feature/book/BookFragment.kt
+++ app/src/main/java/com/eddiej/apisearch/feature/book/BookFragment.kt
@@ -8,8 +8,11 @@ import com.eddiej.apisearch.feature.bookdetail.BookDetailFragment
 import com.eddiej.apisearch.global.ProjectId
 import com.eddiej.apisearch.global.ProjectLayout
 import com.eddiej.apisearch.util.FragmentUtils
+import com.jakewharton.rxbinding4.appcompat.queryTextChanges
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class BookFragment : BaseFragment<FragmentBookBinding>() {
@@ -24,24 +27,26 @@ class BookFragment : BaseFragment<FragmentBookBinding>() {
     override fun setupViews() {
         binding.viewModel = viewModel
 
-        // 입력헤더 어댑터
-        val headerAdapter = BookHeaderAdapter(viewModel)
         // 페이징 어댑터
         bookAdapter = BookAdapter(viewModel)
         bookAdapter.withLoadStateFooter(LoadingStateAdapter(bookAdapter))
 
-        // 2개를 묶어서 같이 쓰자
-        val concatAdapter = ConcatAdapter(headerAdapter, bookAdapter)
-        binding.recyclerView.adapter = concatAdapter
+        binding.recyclerView.adapter = bookAdapter
 
-        // 어댑터 내에서 쿼리결과를 받아와 데이터를 요청
-        viewModel.queryText.observe(this, { query ->
-            viewModel.getList(query)
-                .subscribe {
-                    bookAdapter.submitData(lifecycle, it)
-                }
-                .addTo(disposable)
-        })
+        binding.searchView.queryTextChanges()
+            // 입력 후 1초 경과 시 Query 전달
+            .debounce(1000, TimeUnit.MILLISECONDS, Schedulers.io())
+            .map { charSequence -> charSequence.toString() }
+            // 빈 문자열이나 공백은 무시
+            .filter { query -> !query.isNullOrBlank() }
+            .subscribe { query ->
+                viewModel.getList(query)
+                    .subscribe {
+                        bookAdapter.submitData(lifecycle, it)
+                    }
+                    .addTo(disposable)
+
+            }
 
         // 리스트 내에서 선택한 아이템의 데이터 수신
         viewModel.selectedItem.observe(this, { item ->
--- app/src/main/java/com/eddiej/apisearch/feature/book/BookViewModel.kt
+++ app/src/main/java/com/eddiej/apisearch/feature/book/BookViewModel.kt
@@ -16,18 +16,9 @@ import javax.inject.Inject
 @HiltViewModel
 class BookViewModel @Inject constructor(private val repository: BookRepository) : BaseViewModel() {
 
-    private val _queryText = MutableLiveData<String>()
-    val queryText: LiveData<String>
-        get() = _queryText
-
     private val _selectedItem = MutableLiveData<BookItemEntity>()
     val selectedItem: LiveData<BookItemEntity>
         get() = _selectedItem
-
-    // 검색창에서 받아온 결과 텍스트
-    fun pushQuery(query: String) {
-        _queryText.postValue(query)
-    }
 
     // 상세정보 화면으로 이동
     fun moveToDetail(item: BookItemEntity) {
